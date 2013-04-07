22044cb5a27e918e492e6c91b2d6c6f8bbbccb92
Stanislav Golovanov <stgolovanov@gmail.com>
2013-04-07 13:51:44 +0400

    add strdup func to fix segfault on windows

--- autoload/fuzzycomt.c
+++ autoload/fuzzycomt.c
@@ -36,6 +36,13 @@ void getLineMatches(PyObject* paths, PyObject* abbrev,returnstruct matches[], ch
     }
 }
 
+char *strdup (const char *s) {
+    char *d = malloc (strlen (s) + 1);   // Space for length plus nul
+    if (d == NULL) return NULL;          // No memory
+    strcpy (d,s);                        // Copy the characters
+    return d;                            // Return the new string
+}
+
 char *slashsplit(char *line)
 {
     char *pch, *linedup, *fname;
